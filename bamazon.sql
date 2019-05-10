cdf1585ed871ecc7d316da12ba7aa0ec48912e35
CKayV11 <11303975+ckayv11@users.noreply.github.com>
2019-05-10 16:32:34 -0700

    updated table in database

--- bamazon.sql
+++ bamazon.sql
@@ -14,23 +14,23 @@ CREATE TABLE products (
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ('Sephora Cream Lip Stain Collection Set of 6', 'Beauty & Personal Care', 53.48, 10),
-('Tinted Moisturizer - Nude', 'Beauty & Personal Care', 48.30, 35);
+VALUES ('Sephora Lip Stain Collection', 'Beauty & Personal Care', 53.48, 10),
+('Tinted Moisturizer', 'Beauty & Personal Care', 48.30, 35);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ('Samsung UN65RU7100FXZA Flat 65'' 4K UHD 7 Series Smart TV (2019)', 'Electronics', 697.99, 5),
-('SUPER NINTENDO CLASSIC EDITION CONSOLE Renewed [NINTENDO]', 'Electronics', 129.99, 2);
+VALUES ('Samsung Flat 65-inch Smart TV', 'Electronics', 697.99, 5),
+('Super Nintendo Classic Edition', 'Electronics', 129.99, 2);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ('Shop Succulents Rosette Succulent (Collection of 4)', 'Garden & Outdoor', 18.99, 30),
-('Adjustable Zero Gravity Lounge Chair Recliners', 'Garden & Outdoor', 74.99, 50);
+VALUES ('Succulents (Collection of 4)', 'Garden & Outdoor', 18.99, 30),
+('Adjustable Lounge Chair Recliners', 'Garden & Outdoor', 74.99, 50);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ('YETI Tundra 75 Cooler', 'Sports & Outdoors', 449.99, 4),
-('Black Diamond Momentum Climbing Harness Package - Womens Medium', 'Sports & Outdoors', 99.95, 15);
+('Black Diamond Climbing Harness', 'Sports & Outdoors', 99.95, 15);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ('FurryFriends Soft Sided Pet Crate Travel Carrier Dogs Cats', 'Pet Supplies', 39.99, 60),
-('Wholesome Pride Pet Treats Sweet Potato Fries Dog Treats', 'Pet Supplies', 16.99, 25);
+VALUES ('Pet Crate Travel Carrier', 'Pet Supplies', 39.99, 60),
+('Sweet Potato Fries - Dog Treats', 'Pet Supplies', 16.99, 25);
 
 SELECT * FROM products;
