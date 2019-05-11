362e9c36e9e7a2b1022a0c3085b9c60541e0bfa3
CKayV11 <11303975+ckayv11@users.noreply.github.com>
2019-05-10 17:05:01 -0700

    added function to prompt user for product ID & quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-// const inquirer = require('inquirer');
+const inquirer = require('inquirer');
 const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: process.env.DB_host,
@@ -18,10 +18,42 @@ connection.connect(function(err) {
 function displayProducts() {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
+        console.log("Welcome to Bamazon! Check out our Top 10 products for sale this week!");
+        console.log("");
         for (var i = 0; i < res.length; i++) {
-            console.log('------------------------------------------------------------------------------------------------');
-            console.log('ID#: ' + res[i].id + ' | ' + 'Product: ' + res[i].product_name + ' | ' + 'Price: ' + res[i].price);
-            console.log('------------------------------------------------------------------------------------------------');
+            console.log(res[i].id + ' | ' + res[i].product_name + ' | ' + 'Price: $' + res[i].price);
         }
+        console.log("");
+        selectProduct();
     });
+};
+
+// Function to ask the customer for the ID# & quantity of the product they would like to purchase.
+function selectProduct() {
+    inquirer
+    .prompt([
+      {
+        name: 'productID',
+        type: 'input',
+        message: 'Please enter the ID# of the product you want to buy.',
+        validate: function(value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return ('Please enter a valid number for the item ID.');
+          }
+      },
+      {
+        name: 'quantity',
+        type: 'input',
+        message: 'Please enter the quantity you want to purchase.',
+        validate: function(value2) {
+            if (isNaN(value2) === false) {
+              return true;
+            }
+            return ('Please enter a valid number for the quantity.');
+          }
+      }
+    ])
+
 };
